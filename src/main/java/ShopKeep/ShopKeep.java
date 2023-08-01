e1e304205cd41f787437602f0df57ef035c9b8c6
gvbarker <jonzomb80@gmail.com>
2023-08-01 06:24:54 -0400

    feat: basic bot login setup

--- src/main/java/ShopKeep/ShopKeep.java
+++ src/main/java/ShopKeep/ShopKeep.java
@@ -2,13 +2,21 @@ package ShopKeep;
 
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.object.entity.User;
+import io.github.cdimascio.dotenv.Dotenv;
 import reactor.core.publisher.Mono;
 
 public class ShopKeep {
     public static void main(String[] args) {
-        System.out.println(System.getenv("BOT_TOKEN"));
-        DiscordClient client = DiscordClient.create(System.getenv("BOT_TOKEN"));
-        Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) -> Mono.empty());
+        Dotenv dotenv = Dotenv.load();
+        DiscordClient client = DiscordClient.create(dotenv.get("BOT_TOKEN"));
+        Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) ->
+                gateway.on(ReadyEvent.class, event ->
+                        Mono.fromRunnable(() -> {
+                            final User self = event.getSelf();
+                            System.out.printf("Logged in as %s#%s%n", self.getUsername(), self.getDiscriminator());
+                        })));
         login.block();
     }
 }
