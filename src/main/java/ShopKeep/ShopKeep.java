f9bb52ce774e2eedae312f46685fd0906e2bf1fd
gvbarker <jonzomb80@gmail.com>
2023-08-03 23:13:15 -0400

    feat: add command options and help command

--- src/main/java/ShopKeep/ShopKeep.java
+++ src/main/java/ShopKeep/ShopKeep.java
@@ -8,10 +8,14 @@ import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.User;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
 import org.json.simple.JSONArray;
 import reactor.core.publisher.Mono;
 
 import java.io.File;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,7 +25,11 @@ interface Command {
 public class ShopKeep {
     private static final Map<String, Command> commands = new HashMap<>();
     private static final String cmdPrefix = "!";
+    private static final Options commandOptions = new Options();
     static {
+        commandOptions.addOption("no_weapons", false, "Remove weapons from shop lists.");
+        commandOptions.addOption("no_vehicles", false, "Remove vehicles and mounts from shop lists.");
+        commandOptions.addOption("no_armors", false, "Remove armor pieces from shop lists.");
         commands.put("ping", event -> event.getMessage()
                 .getChannel().block()
                 .createMessage("no").block());
@@ -31,6 +39,17 @@ public class ShopKeep {
                     .createMessage("heard").block();
             SRDHelper testhelper = new SRDHelper(3,0, new String[]{"-no-vehicles"});
             JSONArray[] testarray = testhelper.generateRequestedItems(3);
+        });
+        commands.put("help", event -> {
+            HelpFormatter formatter = new HelpFormatter();
+            StringWriter out = new StringWriter();
+            PrintWriter pw = new PrintWriter(out);
+            formatter.printHelp(pw, 80, "!gen", "ShopKeep", commandOptions,
+                    formatter.getLeftPadding(), formatter.getDescPadding(), "", true);
+            pw.flush();
+            event.getMessage()
+                    .getChannel().block()
+                    .createMessage(out.toString()).block();
         });
     }
     public static void main(String[] args) {
